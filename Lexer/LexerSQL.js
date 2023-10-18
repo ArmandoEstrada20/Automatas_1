dbabc867ca6ecaedc7441228120743dbc0d302f8
Hector Armando Ramirez Estrada <137834404+ArmandoEstrada20@users.noreply.github.com>
2023-10-17 20:23:19 -0700

    Delete Lexer/LexerSQL.js

--- Lexer/LexerSQL.js
+++
@@ -1,4 +0,0 @@
-/*Realizar un programa que analice un achivo, el cual contendrá sentencias SQL.
-El programa leerá las sentencias y las mostrará separadas y en posición vertical,
-respetando la sintaxis de SQL, incluyendo palabras reservadas, operadores, comillas, etc.
-*/
